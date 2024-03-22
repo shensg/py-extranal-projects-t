8b9ce9c6b93fefb0012aa7fd656ee1467f431655
shenshigui@fusen.cn <shenshigui@fusen.cn>
2024-03-22 18:31:33 +0800

    init: sumbit example

---
+++ main.py
@@ -0,0 +1,36 @@
+import shutil
+import subprocess
+import importlib
+
+repo_url = "git@github.com:shensg/packaging_notice.git"
+packaging_dist = "packaging_notice/dist/notice-0.0.1-py3-none-any.whl"
+packaging_path = "packaging_notice"
+
+if __name__ == '__main__':
+    module = importlib.import_module("flask")
+    app = module.Flask(__name__)
+
+
+    @app.route('/', methods=['GET'])
+    def get():
+        print("return success")
+        return {"code": 0, "data": "66666", "msg": "success"}
+
+
+    @app.route('/notice', methods=['GET'])
+    def notice():
+        print("clone external code")
+        subprocess.run(['git', 'clone', repo_url])
+        subprocess.run(['pip', 'install', '--force-reinstall', packaging_dist])
+        shutil.rmtree(packaging_path)
+        module = importlib.import_module("notice")
+        image = "img_v3_0296_533eaea3-47d1-48dc-a5e6-7e315214e05g"
+        title = "通知测试"
+        content = "我们的祖国是花园\n花园的花朵真鲜艳\n阳光和暖的照耀着我们\n每个的脸上的笑开颜！！！！\n"
+        webhook = "d45a291c-b3b8-4058-880f-52c87da38855"
+        module.fs_notice.fs_notice(image, title, content, webhook)
+        # fs_notice(image, title, content, webhook)
+        return {"code": 0, "data": "Send notice success", "msg": "success"}
+
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
